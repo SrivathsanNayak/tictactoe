9f2b4c98c527b0540a28a3799ef9930fe7af7ba9
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-10-13 10:59:23 +0530

    Created boilerplate code for player factory and gameboard module

---
+++ app.js
@@ -0,0 +1,9 @@
+//Player factory function to return player objects
+const player = (playerNumber, playerType, playerTurn) => {
+    return {playerNumber, playerType, playerTurn};
+}
+
+//Gameboard module to return gameboard object
+const gameBoard = (() => {
+
+})();
