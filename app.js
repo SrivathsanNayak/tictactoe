1020e12fec6a2e6250750d8c5d39632c05e0f89b
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-10-14 18:14:19 +0530

    Added event listener for adding X and O to grid

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Player factory function to return player objects
 const player = (playerNumber, playerType, playerMark, playerTurn) => {
-    return {playerNumber, playerType, playerMark, playerTurn};
+    return { playerNumber, playerType, playerMark, playerTurn };
 }
 
 const player1 = player(1, 'human', 'X', true);
@@ -12,10 +12,25 @@ const gameBoard = (() => {
     let squareGridsList = document.querySelectorAll(".square-grid");
     for (let i = 0; i < squareGridsList.length; i++) {
         squareGridsList[i].addEventListener("click", () => {
-            console.log(`Marked Grid ${i}`);
-            board[i] = "Marked";
-            squareGridsList[i].textContent = player1.playerMark;
-            squareGridsList[i].classList.add("square-grid-mark", "player1-mark");
+            if (player1.playerTurn == true && squareGridsList[i].textContent == "") {
+                console.log(`marked Grid ${i}`);
+                board[i] = player1.playerMark;
+                squareGridsList[i].textContent = player1.playerMark;
+                squareGridsList[i].classList.add("square-grid-mark", "player1-mark");
+                squareGridsList[i].style.backgroundColor = "#ddd";
+                player1.playerTurn = false;
+                player2.playerTurn = true;
+            }
+            else if (player2.playerTurn == true && squareGridsList[i].textContent == "") {
+                console.log(`marked Grid ${i}`);
+                board[i] = player2.playerMark;
+                squareGridsList[i].textContent = player2.playerMark;
+                squareGridsList[i].classList.add("square-grid-mark", "player2-mark");
+                squareGridsList[i].style.backgroundColor = "#ddd";
+                player1.playerTurn = true;
+                player2.playerTurn = false;
+            }
+            console.log(board);
         })
     }
 })();
