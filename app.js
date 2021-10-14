7809499bf0692f0c22dea4bd8d797f4813b767b9
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-10-14 15:31:37 +0530

    Modified player factory function, experimenting with gameboard module

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //Player factory function to return player objects
-const player = (playerNumber, playerType, playerTurn) => {
-    return {playerNumber, playerType, playerTurn};
+const player = (playerNumber, playerType, playerMark, playerTurn) => {
+    return {playerNumber, playerType, playerMark, playerTurn};
 }
+
+const player1 = player(1, 'human', 'X', true);
+const player2 = player(2, 'human', 'O', false);
 
 //Gameboard module to return gameboard object
 const gameBoard = (() => {
-
+    let board = [];
+    let squareGridsList = document.querySelectorAll(".square-grid");
+    for (let i = 0; i < squareGridsList.length; i++) {
+        squareGridsList[i].addEventListener("click", () => {
+            console.log(`Marked Grid ${i}`);
+            board[i] = "Marked";
+            // squareGridsList[i].textContent = "X";
+        })
+    }
 })();
