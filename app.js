41f47d7fa0ee0f91d9585a26fa9dcd90f0525743
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-10-15 18:53:32 +0530

    Added logic to check win-loss-tie condition

--- app.js
+++ app.js
@@ -9,42 +9,66 @@ const player2 = player(2, 'human', 'O', false);
 //Gameboard module to return gameboard object
 const gameBoard = (() => {
     let board = [];
+    let movesLeft = 9;
+    let winner = null;
+
     //Node list of all square grids
     let squareGridsList = document.querySelectorAll(".square-grid");
 
     for (let i = 0; i < squareGridsList.length; i++) {
         squareGridsList[i].addEventListener("click", () => {
-            if (player1.playerTurn == true && squareGridsList[i].textContent == "") {
-                console.log(`marked Grid ${i}`);
-                board[i] = player1.playerMark;
-                squareGridsList[i].textContent = player1.playerMark;
-                squareGridsList[i].classList.add("square-grid-mark", "player1-mark");
-                squareGridsList[i].style.backgroundColor = "#ddd";
-                player1.playerTurn = false;
-                player2.playerTurn = true;
+            if (winner == null && movesLeft > 0) {
+                if (player1.playerTurn == true && squareGridsList[i].textContent == "") {
+                    console.log(`marked Grid ${i}`);
+                    board[i] = player1.playerMark;
+                    squareGridsList[i].textContent = player1.playerMark;
+                    squareGridsList[i].classList.add("square-grid-mark", "player1-mark");
+                    squareGridsList[i].style.backgroundColor = "#ddd";
+                    player1.playerTurn = false;
+                    player2.playerTurn = true;
+                }
+                else if (player2.playerTurn == true && squareGridsList[i].textContent == "") {
+                    console.log(`marked Grid ${i}`);
+                    board[i] = player2.playerMark;
+                    squareGridsList[i].textContent = player2.playerMark;
+                    squareGridsList[i].classList.add("square-grid-mark", "player2-mark");
+                    squareGridsList[i].style.backgroundColor = "#ddd";
+                    player1.playerTurn = true;
+                    player2.playerTurn = false;
+                }
+                console.log(board);
+                movesLeft--;
+                console.log(`No. of moves left: ${movesLeft}`);
+                winnerCheck();
             }
-            else if (player2.playerTurn == true && squareGridsList[i].textContent == "") {
-                console.log(`marked Grid ${i}`);
-                board[i] = player2.playerMark;
-                squareGridsList[i].textContent = player2.playerMark;
-                squareGridsList[i].classList.add("square-grid-mark", "player2-mark");
-                squareGridsList[i].style.backgroundColor = "#ddd";
-                player1.playerTurn = true;
-                player2.playerTurn = false;
-            }
-            console.log(board);
         })
     }
 
     //List of all winning positions
     const winConditions = [
-        [0,1,2],
-        [0,3,6], 
-        [0,4,8],
-        [1,4,7],
-        [2,5,8],
-        [3,4,5],
-        [6,7,8],
-        [6,4,2]
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 4, 5],
+        [6, 7, 8],
+        [6, 4, 2]
     ];
+
+    function winnerCheck() {
+        winConditions.forEach((e) => {
+            if (board[e[0]] == "X" && board[e[1]] == "X" && board[e[2]] == "X") {
+                winner = player1.playerNumber;
+                console.log(`Player ${winner} is winner`);
+            }
+            if (board[e[0]] == "O" && board[e[1]] == "O" && board[e[2]] == "O") {
+                winner = player2.playerNumber;
+                console.log(`Player ${winner} is winner`);
+            }
+            if (movesLeft == 0 && winner == null) {
+                console.log("It's a tie");
+            }
+        })
+    }
 })();
